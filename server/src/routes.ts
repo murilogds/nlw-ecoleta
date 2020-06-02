a88e64718cebe6b1d0117895cfc798327744a95d
Murilo Gomes <murilo.goomes@hotmail.com>
2020-06-02 18:19:50 -0300

    Criando rota de cadastro de pontos de coleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { request, response } from 'express';
 import knex from './database/connection';
 
 const routes = express.Router();
@@ -10,10 +10,48 @@ routes.get('/items', async (request, response) => {
     return {
       title: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`,
+      id: item.id
     };
   });
 
   return response.json(serializedItems);
+});
+
+routes.post('/points', async(request, response) =>{
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items
+  } = request.body;
+
+  const trx = await knex.transaction()
+
+  const [id] = await trx('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  });
+
+  const pointItems = items.map((item_id: number) =>{
+    return {
+      item_id,
+      point_id: id
+    }
+  })
+
+  await trx('point_items').insert(pointItems);
+
+  return response.json({success: true});
 });
 
 export default routes;
