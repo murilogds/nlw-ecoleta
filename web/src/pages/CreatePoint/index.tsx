b091b318300550ee163f6aeb17d099b4f97ca4ad
Murilo Gomes <murilo.goomes@hotmail.com>
2020-06-03 15:14:54 -0300

    Carregando localização atual do usuário no mapa

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -33,12 +33,21 @@ const CreatePoint = () => {
   const [selectedUf, setSelectedUf] = useState('0');
   const [selectedCity, setSelectedCity] = useState('0');
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0,0])
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([0,0])
 
   useEffect(() => {
     api.get('items').then(response => {
       setItems(response.data);
     })
   }, []);
+
+  useEffect(() =>{
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+
+      setInitialPosition([latitude, longitude])
+    })
+  })
 
   useEffect(() => {
     axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
@@ -134,7 +143,7 @@ const CreatePoint = () => {
             <span>Selecione o endereço no mapa</span>
           </legend>
 
-          <Map center={[-15.8054859, -48.0307621]} zoom={15} onClick={handleMapClick}>
+          <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
             <TileLayer
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
