ee93aa287ab990c377f42868c10cfb60158a2a80
Murilo Gomes <murilo.goomes@hotmail.com>
2020-06-04 18:45:30 -0300

    Buscando detalhes do ponto de coleta pela API

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,16 +1,49 @@
-import React, { Fragment } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, TouchableOpacity, Image, Text, SafeAreaView } from 'react-native';
 import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
-import Constants from 'expo-constants';
-import { useNavigation } from '@react-navigation/native';
-import { RectButton } from 'react-native-gesture-handler'
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+import api from '../../services/api';
+
+interface Params{
+  point_id: number;
+}
+
+interface Data{
+  point: {
+    name: string;
+    image: string;
+    email: string;
+    whatsapp: string;
+    city: string;
+    uf: string;
+  };
+  items: {
+    title: string;
+  }[]
+}
 
 const Detail = () => {
 
+  const [data, setData] = useState<Data>({} as Data);
+
+  const route = useRoute();
   const navigation = useNavigation();
+
+  const routeParams = route.params as Params;
+
+  useEffect(() => {
+    api.get(`points/${routeParams.point_id}`).then(response => {
+      setData(response.data);
+    });
+  }, [])
 
   function handleNavigationBack() {
     navigation.goBack();
+  }
+
+  if(!data.point){
+    return null;
   }
 
   return (
@@ -22,13 +55,13 @@ const Detail = () => {
           <Icon name="arrow-left" size={20} color="#34cb79" />
         </TouchableOpacity>
 
-        <Image style={styles.pointImage} source={{ uri: "https://images.unsplash.com/photo-1583258292688-d0213dc5a3a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" }} />
-        <Text style={styles.pointName}>Mercado TOP!</Text>
-        <Text style={styles.pointItems}>Lâmpadas, etc.</Text>
+        <Image style={styles.pointImage} source={{ uri: data.point.image }} />
+        <Text style={styles.pointName}>{data.point.name}</Text>
+        <Text style={styles.pointItems}>{data.items.map(item => item.title).join(', ')}</Text>
 
         <View style={styles.address}>
           <Text style={styles.addressTitle}>Endereço</Text>
-          <Text style={styles.addressContent}>Brasília, DF</Text>
+          <Text style={styles.addressContent}>{data.point.city}, {data.point.uf}</Text>
         </View>
       </View>
 
