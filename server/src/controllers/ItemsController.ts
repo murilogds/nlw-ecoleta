8c23d69e9a23db90e2fc046590c51a23296bf317
Murilo Gomes <murilo.goomes@hotmail.com>
2020-06-04 16:29:47 -0300

    Criando página do mapa

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -9,7 +9,7 @@ class ItemsController {
     const serializedItems = items.map(item => {
       return {
         title: item.title,
-        image_url: `http://localhost:3333/uploads/${item.image}`,
+        image_url: `http://192.168.0.7:3333/uploads/${item.image}`,
         id: item.id
       };
     });
