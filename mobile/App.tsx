c94a3b38cd49d6a0f93e5eebc986d7ca688a9f86
Murilo Gomes <murilo.goomes@hotmail.com>
2020-06-04 15:15:53 -0300

    Criando a página home

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Fragment } from 'react';
+import { AppLoading } from 'expo';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto'
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+
+
+import Home from './src/pages/Home';
 
 export default function App() {
+  
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Fragment>
+      <StatusBar barStyle='dark-content' backgroundColor='transparent' translucent/>
+      <Home />
+    </Fragment>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
