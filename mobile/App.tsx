4c1cd11daee612ba8799c9b18f13b8f0e2d67e8d
Murilo Gomes <murilo.goomes@hotmail.com>
2020-06-04 15:31:56 -0300

    Configurando navegação do app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,8 +5,7 @@ import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto'
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
 
-
-import Home from './src/pages/Home';
+import Routes from './src/routes';
 
 export default function App() {
   
@@ -23,7 +22,7 @@ export default function App() {
   return (
     <Fragment>
       <StatusBar barStyle='dark-content' backgroundColor='transparent' translucent/>
-      <Home />
+      <Routes />
     </Fragment>
   );
 }
--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -2,9 +2,15 @@ import React from 'react';
 import { Feather as Icon } from '@expo/vector-icons';
 import { Text, ImageBackground, View, Image, StyleSheet } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
 
+  const navigation = useNavigation();
+
+  function handleNavigateToPoints() {
+    navigation.navigate('Points');
+  }
 
   return (
     <ImageBackground
@@ -19,7 +25,7 @@ const Home = () => {
       </View>
 
       <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={() => {}}>
+        <RectButton style={styles.button} onPress={handleNavigateToPoints}>
           <View style={styles.buttonIcon}>
             <Icon name='arrow-right' color='#FFF' size={24}/>
           </View>
@@ -37,7 +43,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
-    backgroundColor: '#f0f0f5',
   },
 
   main: {
